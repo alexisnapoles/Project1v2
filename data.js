30bb3215c89b96465926994ddff7d7929be3b564
Alexis Dela Torre <alexisndelatorre@gmail.com>
2021-12-13 00:34:55 +0800

    add weather api

--- data.js
+++ data.js
@@ -1,7 +1,125 @@
-async function search(lat, lng, query) {
-    // setup search parameters
-    let ll = lat + ',' + lng;
-    let response = await axios.get(`http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&${query}&callback=FUNCTION_NAME`, {
-        
-    })
-}
+window.onload = function () {
+
+    /**
+     * STEP 2
+     * Set your account access keys when the page and scripts have loaded.
+     */
+    const aeris = new AerisWeather('zCfrYllISK6ZDa81GnpVS', 't1UBi1U14GtUd40ixzqOtLO8uJkJaPD3hhH9e8N5');
+    /**
+     * STEP 3
+     * Start using the library by accessing the available convenience methods on your `AerisWeather` instance.
+     */
+    aeris.api().endpoint('observations').place('seattle,wa').get().then((result) => {
+        const data = result.data.ob;
+        document.querySelector('#status').innerHTML = `The current weather is ${data.weatherPrimary.toLowerCase()} and ${data.tempF} degrees.`;
+    });
+
+};
+
+// jQuery(document).ready(function ($) {
+//     $.ajax({
+//         url: "https://api.aerisapi.com/observations/seattle,wa?client_id=zCfrYllISK6ZDa81GnpVS&client_secret=t1UBi1U14GtUd40ixzqOtLO8uJkJaPD3hhH9e8N5"
+//     })
+//         .done(function (json) {
+//             if (json.success == true) {
+//                 var ob = json.response.ob;
+//                 $('#temperature').html('The current weather in Seattle is ' + ob.weather.toLowerCase() + ' with a temperature of ' + ob.tempF + '&deg;');
+//             }
+//             else {
+//                 alert('An error occurred: ' + json.error.description);
+//             }
+//         });
+// });
+// const WeatherStackApiKey = "cf1786a6ea91cd1a6489b9179560e347";
+
+// const forecast = (latitude, longitude, callback) => {
+//     request(
+//         {
+//             uri: `http://api.weatherstack.com/current?access_key=${WeatherStackApiKey}&query=${latitude},${longitude}`,
+//             json: true,
+//         },
+//         (error, response, body) => {
+//             if (error) {
+//                 callback("Unable to connect to weather service!", undefined);
+//             } else if (body.error) {
+//                 callback("Unable to find location", undefined);
+//             } else {
+//                 callback(undefined, {
+//                     weather_description: body.current.weather_descriptions,
+//                     temperature: body.current.temperature,
+//                     feels_like: body.current.feelslike,
+//                 });
+//             }
+//         }
+//     );
+// };
+// module.exports = data;
+
+// const forecast = (latitude, longitude, callback) => {
+//     const url = `http://api.weatherstack.com/current?access_key=ce59be9657cc9859c578d95311bb1232&query=${latitude},${longitude}`;
+
+//     request({ url, json: true }, (error, response) => {
+//         if (error) {
+//             callback("Unable to connect to weather service", undefined);
+//         } else if (response.body.error) {
+//             callback("Unable to find location", undefined);
+//         } else {
+//             const { temperature, feelslike, weather_descriptions } = response.body.current;
+//             const data = `Current Temperature is: ${temperature} degree. Feels like ${feelslike} degree.\nWeather description: ${weather_descriptions[0]}`;
+//             callback(undefined, data);
+//         }
+//     });
+// };
+
+
+
+// function gettingJSON() {
+//     console.log("jquery loaded");
+//     $.getJSON("api.openweathermap.org/data/2.5/weather?lat=14.5546336&lon=121.015680&appid=ce59be9657cc9859c578d95311bb1232}", function (json) {
+//         console.log(JSON.stringify(json));
+//     });
+// }
+
+// map.on ('load', function () {
+    // let weather = {
+    //     apiKey: 'ce59be9657cc9859c578d95311bb1232',
+    //     fetchWeather: function (city) {
+    //         fetch(
+    //             "https://api.openweathermap.org/data/2.5/weather?q=" +
+    //             city +
+    //             "&units=metric&appid=" +
+    //             this.apiKey
+    //         )
+    //             .then((response) => {
+    //                 if (!response.ok) {
+    //                     alert("No weather found.");
+    //                     throw new Error("No weather found.");
+    //                 }
+    //                 return response.json();
+    //             })
+    //             .then((data) => this.displayWeather(data));
+    //     },
+    //     displayWeather: function (data) {
+    //         const location = data;
+    //         const { temp, humidity } = data.main;
+    //         const { icon, description } = data.weather[0]
+
+    //         document.querySelector('#icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    //         document.querySelector('#location').innerText = location;
+    //         document.querySelector('#temperature').innerText = temp + '°C';
+    //         document.querySelector('#status').innerText = description;
+    //         document.querySelector('#humidity').innerText = humidity + '%';
+    //     },
+    //     search: function () {
+    //         this.fetchWeather(document.querySelector('#geocoder').value);
+    //     }
+    // };
+    // document.querySelector('#geocoder').addEventListener('keyup', function (event) {
+    //     if (event.key == 'Enter') {
+    //         weather.search();
+    //     }
+    // });
+
+// })
+
+
--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
-const locationElement = document.querySelector('[data-location]')
-const temperatureElement = document.querySelector('[data-temperature]')
-const cloudCoverElement = document.querySelector('[data-cloud-cover]')
-const statusElement = document.querySelector('[data-status]')
-
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
     enableHighAccuracy: true
-})
+});
 
 function successLocation(position) {
     setupMap([position.coords.longitude, position.coords.latitude])
-}
+};
+
 
 function errorLocation() {
-    setupMap([14.5546336, 121.015680])
-}
+    setupMap([121.015680, 14.5546336])
+};
+
 
 function setupMap(center) {
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [121.015680, 14.5546336], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: center, // starting position [lng, lat]
+    zoom: 13 // starting zoom
     })
 
     const nav = new mapboxgl.NavigationControl()
@@ -31,12 +28,62 @@ function setupMap(center) {
     const directions = new MapboxDirections({
         accessToken: mapboxgl.accessToken
     })
-
     map.addControl(directions, "top-left")
-    map.addControl(
-        new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-        })
-    );
-}
+
+    const geocoder = new MapboxGeocoder({
+        // Initialize the geocoder
+        accessToken: mapboxgl.accessToken, // Set the access token
+        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+        marker: true, // Do not use the default marker style
+        types: 'country,region,place,postcode,locality,neighborhood,poi',
+        placeholder: 'Enter Location',
+        flyTo: {
+            bearing: 0,
+            // Control the flight curve, making it move slowly and
+            // zoom out almost completely before starting to pan.
+            speed: 0.2, // Make the flying slow.
+            curve: 1, // Change the speed at which it zooms out.
+            // This can be any easing function: it takes a number between
+            // 0 and 1 and returns another number between 0 and 1.
+            easing: function (t) {
+                return t;
+            }
+        },    
+    });
+    document.querySelector('#geocoder').appendChild(geocoder.onAdd(map));
+
+    map.on('load', function () {
+        map.addSource('single-point', {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: []
+            }
+        });
+
+    geocoder.on('result', function (features){
+        console.log(features.result)
+        // map.getSource('single-point').setData(features.result.geometry);
+        // document.querySelector('#location').innerHTML = features.result.text
+    })
+});
+}; 
+
+// const forecast = (latitude, longitude, callback) => {
+//     const url = `http://api.weatherstack.com/current?access_key=cf1786a6ea91cd1a6489b9179560e347d&query=${latitude},${longitude}`;
+
+//     request({ url, json: true }, (error, response) => {
+//         if (error) {
+//             callback("Unable to connect to weather service", undefined);
+//         } else if (response.body.error) {
+//             callback("Unable to find location", undefined);
+//         } else {
+//             const { temperature, humidity, weather_descriptions } = response.body.current;
+//             // document.querySelector('#location').innerText = temperature;
+//             // document.querySelector('#status').innerText = weather_descriptions;
+//             // document.querySelector('#humidity').innerText = humidity;
+//             const data = `Current Temperature is: ${temperature} degree. Feels like ${feelslike} degree.\nWeather description: ${weather_descriptions[0]}`;
+//             callback(undefined, data);
+//         }
+//     });
+// };
